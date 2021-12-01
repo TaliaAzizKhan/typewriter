a46eb149f920025d6b434d9300ab377f013e90d8
Talia Aziz Khan <talyaaziz@gmail.com>
2021-12-01 05:26:14 +0000

    updated

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,14 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs \n";
 
+let delay = 0;
 
 for (const char of sentence) {
-
 
   setTimeout(() => {
     
     // print the char here
     process.stdout.write(char);
-  }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+  }, delay); // <= 1s delay to make it noticeable. Can dial it down later.
+  delay += 50;
 
-  
 }
